538ec0197255ccb5cee48a92238e3e1674a5bb86
Quốc Nam <duongquocnam224400@gmail.com>
2021-03-08 18:35:09 +0700

    'try_glasses_demo'

---
+++ src/components/TryGlasses/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+          <div>
+            <div className="header-tittle">TRY GLASSES ONLINE APP </div>
+          </div>
+        );
+    }
+}
